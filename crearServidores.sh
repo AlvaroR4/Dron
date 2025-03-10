dd31e1490e904326d7b33ae9fd50149e52439962
AlvaroR4 <arv4en2@gmail.com.com>
2025-03-10 17:05:36 +0100

    crear servidores con sh

---
+++ crearServidores.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#chmod +x crearServidores.sh
+
+# Verificar si se proporcionó un número como argumento
+if [ -z "$1" ]; then
+    echo "Uso: $0 <num_instancias>"
+    exit 1
+fi
+
+NUM_INSTANCIAS=$1
+PORT_BASE=50040
+UDP_BASE=14541
+
+for ((i=0; i<NUM_INSTANCIAS; i++)); do
+    PORT=$((PORT_BASE + i))
+    UDP=$((UDP_BASE + i))
+    /home/alvaro/.local/lib/python3.10/site-packages/mavsdk/bin/mavsdk_server -p $PORT udp://:$UDP &
+    echo "Lanzada instancia en puerto $PORT y UDP $UDP"
+done
+
+echo "Todas las instancias han sido lanzadas."
+
