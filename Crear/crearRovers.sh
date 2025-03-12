1e2ae4e3f20ab1dd2ee8f2460c57d088efae8174
Alvaro Ros <149527293+AlvaroR4@users.noreply.github.com>
2025-03-12 12:53:05 +0100

    Update crearRovers.sh

--- Crear/crearRovers.sh
+++ Crear/crearRovers.sh
@@ -17,6 +17,12 @@ if ! [[ "$num_drones" =~ ^[0-9]+$ ]] || [ "$num_drones" -le 0 ]; then
   exit 1
 fi
 
+# Cambiar al directorio de inicio (/home)
+cd /home || { echo "No se pudo acceder al directorio /home"; exit 1; }
+
+# Cambiar al directorio de PX4-Autopilot
+cd alvaro/PX4-Autopilot/ || { echo "No se pudo acceder al directorio /alvaro/PX4-Autopilot/"; exit 1; }
+
 # Ejecutar los drones con el parámetro -i incrementando el número
 for i in $(seq 1 $num_drones); do
   PX4_SYS_AUTOSTART=4009 PX4_SIM_MODEL=gz_r1_rover ./build/px4_sitl_default/bin/px4 -i $i &
