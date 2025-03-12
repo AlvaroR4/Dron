38573c781c9c79d8255dca55b7df9d36bee3a8e6
Alvaro Ros <149527293+AlvaroR4@users.noreply.github.com>
2025-03-12 12:59:00 +0100

    Update crearDrones.sh

--- Crear/crearDrones.sh
+++ Crear/crearDrones.sh
@@ -21,14 +21,14 @@ cd /home || { echo "No se pudo acceder al directorio /home"; exit 1; }
 # Cambiar al directorio de PX4-Autopilot
 cd alvaro/PX4-Autopilot/ || { echo "No se pudo acceder al directorio /alvaro/PX4-Autopilot/"; exit 1; }
 
-# Lanzar el primer dron (esperando 15 segundos después)
-PX4_SYS_AUTOSTART=4001 PX4_SIM_MODEL=gz_x500 ./build/px4_sitl_default/bin/px4 -i 1 &
+# Lanzar el primer dron en una nueva terminal (esperando 15 segundos después)
+xterm -e "PX4_SYS_AUTOSTART=4001 PX4_SIM_MODEL=gz_x500 ./build/px4_sitl_default/bin/px4 -i 1; bash" &
 echo "Iniciando el primer dron con ID 1"
 sleep 15  # Esperar 15 segundos antes de continuar con el resto
 
-# Ejecutar el resto de los drones con el parámetro -i incrementando el número
+# Ejecutar el resto de los drones en nuevas terminales
 for i in $(seq 2 $num_drones); do
-  PX4_SYS_AUTOSTART=4001 PX4_SIM_MODEL=gz_x500 ./build/px4_sitl_default/bin/px4 -i $i &
+  xterm -e "PX4_SYS_AUTOSTART=4001 PX4_SIM_MODEL=gz_x500 ./build/px4_sitl_default/bin/px4 -i $i; bash" &
   echo "Iniciando dron con ID $i"
 done
 
